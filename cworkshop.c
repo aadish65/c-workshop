e830a927ce325354cae7c0ab4be1c0784dbfac66
aadish65 <aadishprabhu01@gmail.com>
2025-12-01 11:40:11 +0530

    hi

---
+++ cworkshop.c
@@ -0,0 +1,6 @@
+#include <stdio.h>      // standard input/output header
+
+int main() {            // main function: program starts here
+    printf("Hello, World!\n");   // print a message
+    return 0;           // tell OS that program finished successfully
+}
